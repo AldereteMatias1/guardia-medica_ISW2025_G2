2bed81e92ca231a6aa4a7eece27f5a0fdb2c343f
ivanochoa229 <ivanochoa229@gmail.com>
2025-11-07 19:24:01 -0300

    resolviendo dependencia faltante

--- src/models/paciente/paciente.module.ts
+++ src/models/paciente/paciente.module.ts
@@ -3,6 +3,8 @@ import { PACIENTE_REPOSITORIO } from '../../app/interfaces/patient.repository';
 import { PatientRepositoryImpl } from '../../persistence/patient.repository';
 import { SERVICIO_PACIENTE } from 'src/app/interfaces/paciente.service';
 import { PacienteServicio } from 'src/app/services/paciente.service';
+import { REPOSITORIO_OBRA_SOCIAL } from 'src/app/interfaces/obra.social.repository';
+import { ObraSocialRepositorio } from 'src/persistence/obra.social.repository';
 
 
 @Module({
@@ -14,6 +16,10 @@ import { PacienteServicio } from 'src/app/services/paciente.service';
     {
       provide: SERVICIO_PACIENTE,
       useClass: PacienteServicio
+    },
+    {
+      provide: REPOSITORIO_OBRA_SOCIAL,
+      useClass: ObraSocialRepositorio
     }
   ],
   exports: [PACIENTE_REPOSITORIO, SERVICIO_PACIENTE],
